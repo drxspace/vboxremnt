66b39e5b199ed99d1c2f47a4bdc532719077fb16
John A Ginis <drxspace@gmail.com>
2014-12-29 11:29:14 +0200

    refresh icon cache

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ cd "$(dirname "$0")"
 
 [[ -f ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs ]] && source ${XDG_CONFIG_HOME:-~/.config}/user-dirs.dirs
 [[ ! -f "${XDG_DESKTOP_DIR:-$HOME/Desktop}"/remvbox.desktop ]] && {
-  cp -v remvbox.desktop "${XDG_DESKTOP_DIR:-$HOME/Desktop}"
-  chmod +x "${XDG_DESKTOP_DIR:-$HOME/Desktop}"/remvbox.desktop
+  cp -v remvbox.desktop "${XDG_DESKTOP_DIR:-$HOME/Desktop}";
+  chmod +x "${XDG_DESKTOP_DIR:-$HOME/Desktop}"/remvbox.desktop;
 }
 
 if [[ $EUID -ne 0 ]]; then
@@ -31,10 +31,12 @@ fi
 	cp -v remvbox /usr/local/bin/ &&
 	tar xzvf remvbox_icons.tar.gz -C /usr/share/icons/ &&
 	chown -R root:root /usr/share/icons/hicolor/ &&
-	desktop-file-install remvbox.desktop
+	{ ls -1 /usr/share/icons/*/{.,}icon-theme.cache 2>/dev/null | sudo xargs -n1 rm -f;
+	  ls -d1 /usr/share/icons/*/ | sudo xargs -n1 gtk-update-icon-cache -ft; }
+	desktop-file-install remvbox.desktop;
 } || {
-	notify-send "VBox Remounter" "Installation's failed..." -i face-worried
-	exit 1
+	notify-send "VBox Remounter" "Installation's failed..." -i face-worried;
+	exit 1;
 }
 
 notify-send "VBox Remounter" "Installation's done okay\!" -i face-wink
